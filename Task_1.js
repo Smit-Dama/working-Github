a0767ac0d6956b5c544d8318fd93b1cfeda85fa5
sagar110599 <sagardama99@gmail.com>
2022-09-21 19:23:02 +0530

    Task 1 Completed

---
+++ Task_1.js
@@ -0,0 +1,25 @@
+const readline = require("readline-sync")
+let n = Number(readline.question("Enter number of words : "))
+console.log("There are " + n + "  Words")
+var words=[]
+for(let i=0;i<n;i++)
+{
+    let word=readline.question("Enter the $(words[i]) :")
+    words.push(word)
+}
+
+
+    console.log(words)
+
+const count = {}
+
+for (let index = 0; index < words.length; index++) {
+  const element = words[index]
+
+  if (count[element]) {
+    count[element] += 1
+  } else {
+    count[element] = 1
+  }
+}
+console.log(count)
